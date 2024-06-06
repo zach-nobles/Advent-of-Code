01bdcfc332272a0c25d7700f839e4b33ebd0e71e
Zach Nobles <89807032+zach-nobles@users.noreply.github.com>
2024-06-06 10:44:28 -0700

    Changing color of output text for readability.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@
 #include "Challenges/Day_04/Day_04.h"
 #include "Challenges/Day_06/Day_06.h"
 #include "Challenges/Day_07/Day_07.h"
+#include "Challenges/Day_08/Day_08.h"
 
 using namespace std;
 
+constexpr string set_text_yellow() {
+    return "\033[33m";
+}
+
+constexpr string set_text_white() {
+    return "\033[0m";
+}
+
 int main() {
-    cout << endl << "[RUNNING] Advent of Code Project" << endl << endl;
-    Day_07b();
-    cout << "\n[COMPLETED] Advent of Code Project" << endl << endl;
+    cout << endl << set_text_yellow() << "[RUNNING] Advent of Code Project" << set_text_white() << endl << endl;
+    Day_00();
+    cout << set_text_yellow() << "\n[COMPLETED] Advent of Code Project" << set_text_white() << endl << endl;
     return 0;
 }
