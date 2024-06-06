61e8077a9fc1202e3255ea56c3e9c942f796ab4b
Varit Vichathorn (Ohm) <96359857+omezzvct@users.noreply.github.com>
2024-06-05 22:14:37 -0700

    WIP Day01 b

--- Challenges/Day_01/Day_01.cpp
+++ Challenges/Day_01/Day_01.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <algorithm> 
 
 using namespace std;
 
 vector<string> lines;
+
 
 void importText(string fileName)
 {
@@ -20,26 +23,78 @@ void importText(string fileName)
 int getValue(vector<string>  lines)
 {
   int val = 0;
+  string val_l = "";
+  string val_r = "";
+
+  map<string,string> mp;
+  mp["one"] = "1";
+  mp["two"] = "2";
+  mp["three"] = "3";
+  mp["four"] = "4";
+  mp["five"] = "5";
+  mp["six"] = "6";
+  mp["seven"] = "7";
+  mp["eight"] = "8";
+  mp["nine"] = "9";
 
   for(auto line : lines)
   {
-    int l = 0;
-    int r = line.size()-1;
+    // LHS
+    for(size_t i=0; i<line.size();i++)
+    {
+      if(!isdigit(line[i]))
+      {
+        for(auto pair : mp)
+        {
+          if(pair.first == line.substr(i,pair.first.size()))
+          {
+            val_l = pair.second;
+            break;
+          }
+        }
+      }
+      else if(isdigit(line[i]))
+      {
+        val_l = line.substr(i,1);
+        break;
+      }
+    }
+    // RHS
+    string revLine = line;
+    reverse(revLine.begin(),revLine.end());
 
-    while(!isdigit(line[l]) && l < r)
-    { 
-      l++;
+    map<string,string> revmp;
+
+    for(auto pair : mp)
+    {
+      string revmpKey = pair.first;
+      reverse(revmpKey.begin(),revmpKey.end());
+      revmp[revmpKey] = pair.second;
     }
-    while(!isdigit(line[r]) && l < r)
-    { 
-      r--;
+
+    for(size_t i=0; i<revLine.size();i++)
+    {
+      if(!isdigit(revLine[i]))
+      {
+        for(auto pair : revmp)
+        {
+          if(pair.first == revLine.substr(i,pair.first.size()))
+          {
+            val_r = pair.second;
+            break;
+          }
+        }
+      }
+      else if(isdigit(revLine[i]))
+      {
+        val_r = line.substr(i,1);
+        break;
+      }
     }
-    string val_l = line.substr(l,1);
-    string val_r = line.substr(r,1);
     string valStr = val_l + val_r;
+    cout<<"val_l = "<<val_l<<" | val_r = "<<val_r<<"\n";
     val += stoi(valStr);
   }
-
   return val;
 }
 
@@ -47,5 +102,4 @@ void Day_01()
 {
   importText("Challenges/Day_01/input.txt");
   cout<<getValue(lines)<<"\n";
-
 }
